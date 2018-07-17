d57377cc4701854c685f33a09d25b56632cc69f8
Paulo Valente <polvalente@gmail.com>
2018-07-17 10:22:10 -0300

    feature: good submission in report

--- linear_regression/regressor.py
+++ linear_regression/regressor.py
@@ -13,12 +13,34 @@ class Regressor():
     def __init__(self, X, y, kind):
         self.X = X
         self.y = y
-        if kind.upper() == 'SVM':
+        kind = kind.upper()
+        if kind == 'SVM':
             from sklearn import svm
             self.regressor = svm.SVR()
+        elif kind == 'RIDGECV':
+            from sklearn import linear_model
+            self.regressor = linear_model.RidgeCV(
+                alphas=[
+                    x * y for x in [0.01, 0.1, 1, 10] for y in [1, 5]
+                    ]
+                )
+        elif kind == 'SVM_GRID':
+            from sklearn import svm
+            from sklearn.model_selection import GridSearchCV
+            self.regressor = GridSearchCV(
+                svm.SVR(),
+                {
+                    'C': [1e0, 5e0, 1e1, 5e1, 1e2, 5e2, 1e3],
+                    'epsilon': [1e-3, 1e-2, 1e-1, 1e0],
+                    'kernel': ['linear', 'rbf', 'poly'],
+                    'degree': [2, 3, 4]
+                },
+                scoring='neg_mean_squared_log_error'
+            )
 
     def train(self):
         self.regressor.fit(self.X, self.y)
 
     def predict(self, X_test):
+        print(self.regressor)
         return self.regressor.predict(X_test)
